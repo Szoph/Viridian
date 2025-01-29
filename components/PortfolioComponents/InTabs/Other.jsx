ad61d3dd1579dfef9e08dd45816a34ef648ea80f
Sophie <szoph101@hotmail.co.uk>
2025-01-29 14:53:01 +0000

    added more pictures to staircases and other

--- components/PortfolioComponents/InTabs/Other.jsx
+++ components/PortfolioComponents/InTabs/Other.jsx
@@ -1,10 +1,16 @@
 'use client'
 const Other = () => {
     const images = [
+        '/Other/Other11.jpg',
+        '/Other/Other12.jpg',
+        '/Other/Other13.jpg',
+        '/Other/Other14.jpg',
         '/Other/Other4.jpg',
         '/Other/Other10.jpg',
         '/Other/Other3.jpg',
         '/Other/Other2.jpg',
+        '/Other/Other15.jpg',
+        '/Other/Other16.jpg',
         '/Other/Other5.jpg',
         '/Other/Other6.jpg',
         '/Other/Other7.jpg',
