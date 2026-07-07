8c94b569ed2d7114f5d93568c9a0f9bd6e5bc442
Sophie Woodcock <szoph101@hotmail.co.uk>
2026-07-07 17:45:36 +0100

    Changed styling on contact page

--- components/ContactComponents/ContactPage.jsx
+++ components/ContactComponents/ContactPage.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import AlternativeContact from './AlternativeContact'
 import Form from './Form'
 
+
 const ContactPage = () => {
   return (
-    <div className='md:px-24 lg:px-24 px-6 py-10 w-full move-up'>
-        <h1 className='font-bold text-4xl mb-24 text-center heading text-[#55342B]'>Contact Us</h1>
-    <div className='flex md:flex-col lg:flex-row gap-3 flex-col justify-center'>
+    <div className='md:px-24 lg:px-24 px-6 pt-24 w-full move-up'>
+        <h1 className='font-bold text-5xl mb-24 text-center heading text-[#55342B] tracking-wider'>Contact Us</h1>
+    <div className='flex md:flex-col lg:flex-row gap-3 pt-16 flex-col justify-center'>
         <Form />
         <AlternativeContact /> 
     </div>
--- components/ContactComponents/Form.jsx
+++ components/ContactComponents/Form.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import { useForm, ValidationError } from '@formspree/react'
+import { Mail, Phone } from 'lucide-react'
 
 const Form = () => {
     const [state, handleSubmit] = useForm('xnqeepgl');
@@ -14,8 +15,9 @@ const Form = () => {
 
 
   return (
-    <div className='md:w-full lg:w-1/2 w-full'>
-        <h4 className='text-xl w-3/4'>Feel free to contact us any time. We will get back to you as soon as possible.</h4>
+    <div className='md:w-full lg:w-1/2 w-full tracking-wide'>
+        <h4 className='text-2xl w-3/4 font-medium tracking-wider'>Feel free to contact us to discuss your project</h4>
+        <h4 className='text-lg font-normal w-full md:w-full lg:w-3/4'>We offer a free, no-obligation quote following an in-person site visit, allowing us to fully understand your requirements and provide accurate pricing. </h4>
     {/* https://formspree.io/f/xnqeepgl */}
 
     { state.errors && state.errors.length > 0 && (
@@ -28,24 +30,104 @@ const Form = () => {
     <div className="flex flex-col">
     
     <div className="my-4">
-    <input type="text" className="form-control px-4 py-2 shadow-md rounded-md w-full placeholder:text-black font-semibold" placeholder="Name" name="name" aria-label="Name" required />
-    <ValidationError prefix="Name" field="name" errors={state.errors} />
+      <label htmlFor="name" className="block mb-2 font-medium text-black pt-3 text-md">Name *</label>
+    <input type="text" className="w-full rounded-md border border-gray-300 px-4 py-2 shadow-md focus:outline-none focus:ring-2 focus:ring-[#D8B689] focus:border-[#D8B689]"  
+    name="name" 
+    aria-label="Name"
+    id='name' 
+    required />
+    {/* <ValidationError prefix="Name" field="name" errors={state.errors} /> */}
   </div>
 
   
   <div className="my-4">
-    <input type="email" className="form-control px-4 py-2 shadow-md rounded-md w-full 6 placeholder:text-black font-semibold" placeholder="Email" name="email" aria-label="Email" required />
-    <ValidationError prefix="Email" field="email" errors={state.errors} />
-  </div>
+  <label 
+    htmlFor="email" 
+    className="font-medium text-black text-md block mb-2"
+  >
+    Email Address *
+  </label>
 
+  <div className="flex items-center border rounded-md shadow-md px-4 py-2 focus-within:ring-2 focus-within:ring-[#D8B689]
+                focus-within:border-[#D8B689]">
+    <Mail size={24} strokeWidth={2} className="text-[#55342B] opacity-50" />
+
+    <input
+      id="email"
+      type="email"
+      autoComplete="email"
+      className="grow ml-2 outline-none bg-transparent"
+      required
+    />
+  </div>
+</div>
+
+
+<div className="my-4">
+  <label 
+    htmlFor="number" 
+    className="font-medium text-black text-md block mb-2"
+  >
+    Phone Number
+  </label>
+
+  <div className="flex items-center border rounded-md shadow-md px-4 py-2 focus-within:ring-2 focus-within:ring-[#D8B689]
+                focus-within:border-[#D8B689]">
+    <Phone size={24} strokeWidth={2} className="text-[#55342B] opacity-50" />
+
+    <input
+      id="number"
+      type="tel"
+      autoComplete="tel"
+      className="grow ml-2 outline-none bg-transparent "
+      
+    />
+  </div>
+</div>
+
+
+
+
+  <div className="my-4">
+    <label htmlFor='projectType' className='block font-medium text-black mb-2 text-md'>Project Type *</label>
+    <select required id='projectType' defaultValue='' className='w-full rounded-md border border-gray-300 px-4 py-2 shadow-md focus:outline-none focus:ring-2 focus:ring-[#D8B689] focus:border-[#D8B689]'>
+      <option value='' disabled>Please select</option>
+      <option value='Kitchen'>Kitchen</option>
+      <option value='Media Wall'>Media Wall</option>
+      <option value='Staircase'>Staircase</option>
+       <option value='Fitted Wardrobe'>Fitted Wardrobe</option>
+       <option value='Bespoke Cabinetry & Storage'>Bespoke Cabinetry & Storage</option>
+      <option value='Other'>Other</option>
+    </select>
+    {/* <ValidationError prefix="Message" field="message" errors={state.errors} /> */}
+  </div>
  
-  <div className="my-4">
-    <input type="text" className="form-control px-4 py-2 shadow-md rounded-md w-full placeholder:text-black font-semibold" placeholder="Message" name="message" aria-label="Message" required />
-    <ValidationError prefix="Message" field="message" errors={state.errors} />
-  </div>
+ <div className='my-4'>
+  <label htmlFor='projectDetails' className='block font-medium text-black. mb-2 text-md'>Tell us about your project</label>
+  <textarea id='projectDetails' rows={4}  className='w-full rounded-md border border-gray-300 px-4 py-3 shadow-md resize-y focus:outline-none focus:ring-2 focus:ring-[#D8B689] focus:border-[#D8B689]'/>
+ </div>
+
 
   <div className="my-4">
-    <button type='submit' disabled={state.submitting} className='w-full p-4 shadow-md rounded-md font-bold text-white bg-[#99582a] hover:opacity-75'>S E N D</button>
+    <label htmlFor='heardAbout' className='block font-medium text-black mb-2 text-md'>How did you hear about us? </label>
+    <select id='heardAbout' className='w-full rounded-md border border-gray-300 px-4 py-2 shadow-md focus:outline-none focus:ring-2 focus:ring-[#D8B689] focus:border-[#D8B689]'>
+      <option value=''>Please select</option>
+      <option value='Google'>Google</option>
+      <option value='Facebook'>Facebook</option>
+      <option value='Instagram'>Instagram</option>
+       <option value='Recommendation'>Recommendation</option>
+      <option value='Other'>Other</option>
+    </select>
+    {/* <ValidationError prefix="Message" field="message" errors={state.errors} /> */}
+  </div>
+
+  
+
+
+  
+
+  <div className="my-4">
+    <button type='submit' disabled={state.submitting} className='w-full p-4 shadow-md rounded-md font-bold text-white bg-[#D8B689] hover:opacity-75'>S E N D</button>
   </div>
 </div>
 
